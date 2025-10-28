0853b4e00194a29722ec7d37596fbf0c64aa0e1e
Fahad Masroor <fahadmasrur786@gmail.com>
2025-10-28 21:15:47 +0530

    Delete backend directory

--- backend/api/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import BorrowerList, LoanCreate, PredictOutcome
-
-urlpatterns = [
-    path("borrowers/", BorrowerList.as_view(), name="borrower_list"),
-    path("loans/", LoanCreate.as_view(), name="loan_create"),
-    path("predict/", PredictOutcome.as_view(), name="predict_outcome"),
-]
